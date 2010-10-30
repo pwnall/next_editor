7207d51d732517d05f193a777184589117c97989
Victor Costan <costan@gmail.com>
2010-10-30 11:09:27 -0400

    Started getting the Water UI rolling.

--- src/next_editor/ui_water.js
+++ src/next_editor/ui_water.js
@@ -6,58 +6,93 @@
  *   tokenizer:: conforms to the NextEditor.Tokenizer interface for deciding
  *               which parts of the text get highlighted
  */
-NextEditor.UI.Water = function(editorElement, inputElement, formElement) {
-  var e = this;
-
-  /** The DOM element with contentEditable=true. */
-  e.editorElement = editorElement;
-  /** The textarea or input backing the element's contents. */
-  e.inputElement = inputElement;
-  /** The form to be submitted when the user presses Enter. */
-  e.formElement = formElement;
-  
-  /** The contents of the element last time we did highlighting. */
-  e.oldContent = null;
-  
-  e.applyEditorStyle = function() {
-    var matching_styles = ['padding', 'margin', 'border-left-width',
-                           'border-right-width', 'border-top-width',
-                           'border-bottom-width',
-                           'height', 'width', 'font-family', 'font-size'];
-    var style_hash = new Array();
-    var inputElement = $(e.inputElement);
-    var editorElement = $(e.editorElement);
-    for (var i = 0; i < matching_styles.length; i++) {
-      style_hash[matching_styles[i]] = editorElement.css(matching_styles[i]);
-    }
-    inputElement.css(style_hash);        
-    editorElement.css({'z-index' : -5});
-    editorElement.attr('contentEditable', 'false');
-    $(editorElement).before(
-        '<style type="text/css">.no, .may, .yes { padding: 0 !important; }</style>');
-    
-    inputElement.css({'background-color' : 'transparent',
-                      'opacity' : 0.0,
-                      'visibility' : 'visible',
-                      'position' : 'absolute',
-                      'left' : '4px',
-                      'top' : '3px'});
+NextEditor.UI.Water = function(options) {
+  this.inputElement = options.inputElement;
+  if (!this.inputElement) {
+    window.console && console.error("No input elment given! noop");
+    return;
   }
-  e.applyEditorStyle();
-
-  /** Parses the editor content to make it nice, only if it changed. */
-  e.contentMayHaveChanged = function() {
-    var text = $(e.inputElement).val();
-    if (e.oldContent == text) return;
-    
-    var formattedData = NextEditor.Parsing.formatText(text, null);
-    $(e.editorElement).html('');
-    for (var i = 0; i < formattedData.nodes.length; i++) {
-      e.editorElement.appendChild(formattedData.nodes[i]);
-    }
-      
-    e.oldContent = text;
+  this.tokenizer = options.tokenizer; 
+  if (!this.tokenizer) {
+    window.console && console.error("No tokenizer given! noop");
+    return;
   }
-  e.contentMayHaveChanged();
+  
+  this.formSubmitter = options.formSubmitter;
+  this.buildEditor();
+};
+
+/** The textarea or input backing the element's contents. */
+NextEditor.UI.Water.prototype.inputElement = null;
+
+/** The form to be submitted when the user presses Enter. */
+NextEditor.UI.Water.prototype.formElement = null;
+
+/** The DOM element with contentEditable=true. */
+NextEditor.UI.Water.prototype.editorElement = null;
+
+/** Constructs the editor DOM UI. */
+NextEditor.UI.Water.prototype.buildEditor = function() {
+  this.editorElement = document.createElement('div');
+  
+  var matching_styles = ['padding', 'margin', 'border-left-width',
+                         'border-right-width', 'border-top-width',
+                         'border-bottom-width',
+                         'height', 'width', 'font-family', 'font-size'];
+  var style_hash = new Array();
+  for (var i = 0; i < matching_styles.length; i++) {
+    style_hash[matching_styles[i]] =
+        $(this.inputElement).css(matching_styles[i]);
+  }
+  $(this.editorElement).css(style_hash);
+  $(this.editorElement).css({'z-index' : -5});
+
+  $(this.inputElement).css({'background-color': 'transparent', 'opacity': 0.0,
+      'visibility': 'visible', 'position': 'absolute', 'left': '4px',
+      'top': '3px'});
+  $(this.inputElement).after(this.editorElement);
+  
+  this.onPossibleChange();
+};
+
+/** The contents of the editor element last time we did highlighting. */
+NextEditor.UI.Water.prototype.oldContent = null;
+
+/** Parses the editor content to make it nice, only if it changed. */
+NextEditor.UI.Water.prototype.onPossibleChange = function() {
+  var text = $(this.inputElement).attr('value');
+  if (this.oldContent == text) return;
+    
+  var tokens = this.tokenizer.tokenize(text);
+  var domData = NextEditor.DOM.buildDom(tokens, null);  
+
+  this.setEditorContent(domData);    
+  this.oldContent = text;
+};
+
+/** Updates the editor UI to reflect content change
+ *
+ * Args:
+ *   domData:: the new DOM contents and selection information for the editor UI
+ */
+NextEditor.UI.Water.prototype.setEditorContent = function(domData) {  
+  this.editorElement.innerHTML = '';
+  for (var i = 0; i < domData.nodes.length; i++) {
+    this.editorElement.appendChild(domData.nodes[i]);
+  }
+};
+
+/** Submits the editor's form if the user presses Enter. */
+NextEditor.UI.Water.prototype.onSubmitKey = function() {
+  this.formSubmitter.submit();
+};
+
+/** The DOM element receiving user input events. */
+NextEditor.UI.Water.prototype.eventSource = function() {
+  return this.inputElement;
 }
 
+/** True if no change events should be generated when an IME UI is active. */
+NextEditor.UI.Water.prototype.needsImeSupport = function() {
+  return true;
+}
